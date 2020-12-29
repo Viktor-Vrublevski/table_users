3055a7e37856cf1ec1c60976090f376fb6b7ad6e
Viktor <53341437+Wiktor1-bot@users.noreply.github.com>
2020-12-29 21:06:54 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-       UserServiceImpl service = new UserServiceImpl();
+       UserService service = new UserServiceImpl();
 
 
         service.createUsersTable();
