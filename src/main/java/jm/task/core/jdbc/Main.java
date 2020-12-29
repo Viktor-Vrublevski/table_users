fe15a273fa099f5e1d77f92ce559a927364cff0e
Viktor1 <zestaw2017@gmail.com>
2020-12-29 20:48:05 +0300

    fixed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc;
 
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl service = new UserServiceImpl();
+        UserService service = new UserServiceImpl();
 
         service.createUsersTable();
 
